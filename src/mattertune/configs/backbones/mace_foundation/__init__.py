5bc07f0ca474a58f34854358a121ea4217c9f244
LingyuKong <lkong88@gatech.edu>
2025-06-09 04:39:38 -0400

    fix mace bug for partition

---
+++ src/mattertune/configs/backbones/mace_foundation/__init__.py
@@ -0,0 +1,18 @@
+__codegen__ = True
+
+from mattertune.backbones.mace_foundation.model import FinetuneModuleBaseConfig as FinetuneModuleBaseConfig
+from mattertune.backbones.mace_foundation import MACEBackboneConfig as MACEBackboneConfig
+
+from mattertune.backbones.mace_foundation.model import FinetuneModuleBaseConfig as FinetuneModuleBaseConfig
+from mattertune.backbones.mace_foundation import MACEBackboneConfig as MACEBackboneConfig
+
+from mattertune.backbones.mace_foundation.model import backbone_registry as backbone_registry
+
+from . import model as model
+
+__all__ = [
+    "FinetuneModuleBaseConfig",
+    "MACEBackboneConfig",
+    "backbone_registry",
+    "model",
+]
