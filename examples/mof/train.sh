56d658e7b6ca21cf44e3f7f5f3f8addbcd2c8254
LingyuKong <lkong88@gatech.edu>
2025-08-25 12:28:59 -0400

    train and test scripts for mof

---
+++ examples/mof/train.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Source the conda.sh script to enable 'conda' command
+source /net/csefiles/coc-fung-cluster/lingyu/miniconda3/etc/profile.d/conda.sh
+
+
+conda activate mattersim-tune
+batch_size=24
+python mof_train.py \
+    --model_type "mattersim-1m" \
+    --devices 0 1 2 3 4 5 6 7 \
+    --conservative
+
+python mof_train.py \
+    --model_type "mattersim-5m" \
+    --devices 0 1 2 3 4 5 6 7 \
+    --conservative
+
+conda activate mace-tune
+batch_size=24
+python mof_train.py \
+    --model_type "mace-medium-omat-0" \
+    --devices 0 1 2 3 4 5 6 7 \
+    --conservative
+
+conda activate jmp-tune
+batch_size=8
+python mof_train.py \
+    --model_type "jmp-s" \
+    --devices 0 1 2 3 4 5 6 7 \
+    --conservative
